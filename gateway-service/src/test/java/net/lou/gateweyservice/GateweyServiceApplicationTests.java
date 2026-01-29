f1759e4143af3a35efce515bc14bdb269c6d18dc
leriaetnasta <leriaetnasta@users.noreply.github.com>
2026-01-29 13:09:45 +0100

    init

---
+++ gateway-service/src/test/java/net/lou/gateweyservice/GateweyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package net.lou.gateweyservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GateweyServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
